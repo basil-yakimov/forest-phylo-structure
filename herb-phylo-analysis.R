d7d88660a56f5f170ea61c28a77e3b835526e356
basil-yakimov <damselfly@yandex.ru>
2018-10-18 23:54:10 +0300

    fix herb analysis

--- herb-phylo-analysis.R
+++ herb-phylo-analysis.R
@@ -10,7 +10,7 @@ mpd.mat.a <- mpd(ha, cophenetic(tree), abundance.weighted = T)
 
 #_____________________________________________________________________
 
-ses <- ses.mpd(ha, dist.mat, null.model = "taxa.labels", runs = 999, iterations = 1000)
+ses <- ses.mpd(ha, cophenetic(tree), null.model = "taxa.labels", runs = 999, iterations = 1000)
 
 plot(1:96, ses$mpd.obs)
 plot(1:96, ses$mpd.obs.z)
