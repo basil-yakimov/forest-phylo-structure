557e9cac6de05bbbb27f28d1b9f3f03c8fac8ceb
arrirh <arrirh@gmail.com>
2018-11-05 20:10:25 +0300

    Other null models are implemented.

--- herb-phylo-analysis.R
+++ herb-phylo-analysis.R
@@ -2,16 +2,86 @@ load('herb-phylo.RData')
 
 library(ape)
 library(picante)
-mpd.mat <- mpd(ha, cophenetic(tree), abundance.weighted = F)
+
+dist.mat <- cophenetic(tree)
 
 #_____________________________________________________________________
 
-mpd.mat.a <- mpd(ha, cophenetic(tree), abundance.weighted = T)
+hses.tl <- ses.mpd(ha, dist.mat, null.model = "taxa.labels", runs = 999, iterations = 1000)
+
+hses.r <- ses.mpd(ha, dist.mat, null.model = "richness", runs = 999, iterations = 1000)
+
+hses.f <- ses.mpd(ha, dist.mat, null.model = "frequency", runs = 999, iterations = 1000)
+
+hses.sp <- ses.mpd(ha, dist.mat, null.model = "sample.pool", runs = 999, iterations = 1000)
+
+hses.pp <- ses.mpd(ha, dist.mat, null.model = "phylogeny.pool", runs = 999, iterations = 1000)
+
+hses.is <- ses.mpd(ha, dist.mat, null.model = "independentswap", runs = 999, iterations = 1000)
+
+hses.ts <- ses.mpd(ha, dist.mat, null.model = "trialswap", runs = 999, iterations = 1000)
 
 #_____________________________________________________________________
 
-ses <- ses.mpd(ha, cophenetic(tree), null.model = "taxa.labels", runs = 999, iterations = 1000)
+hses.a.tl <- ses.mpd(ha, dist.mat, null.model = "taxa.labels", runs = 999, iterations = 1000, abundance.weighted = T)
 
-plot(1:96, ses$mpd.obs)
-plot(1:96, ses$mpd.obs.z)
-plot(1:96, ses$mpd.obs.p)
+hses.a.r <- ses.mpd(ha, dist.mat, null.model = "richness", runs = 999, iterations = 1000, abundance.weighted = T)
+
+hses.a.f <- ses.mpd(ha, dist.mat, null.model = "frequency", runs = 999, iterations = 1000, abundance.weighted = T)
+
+hses.a.sp <- ses.mpd(ha, dist.mat, null.model = "sample.pool", runs = 999, iterations = 1000, abundance.weighted = T)
+
+hses.a.pp <- ses.mpd(ha, dist.mat, null.model = "phylogeny.pool", runs = 999, iterations = 1000, abundance.weighted = T)
+
+hses.a.is <- ses.mpd(ha, dist.mat, null.model = "independentswap", runs = 999, iterations = 1000, abundance.weighted = T)
+
+hses.a.ts <- ses.mpd(ha, dist.mat, null.model = "trialswap", runs = 999, iterations = 1000, abundance.weighted = T)
+
+rm(my.add, my.bind.tip)
+save.image("herb-phylo-analysis-ses.RData")
+
+#_____________________________________________________________________
+
+plot(1:96, hses.tl$mpd.obs)
+plot(1:96, hses.tl$mpd.obs.z)
+
+h.ses <- data.frame(n = 1:96, taxa.labels = hses.tl$mpd.obs.z,
+                    richness = hses.r$mpd.obs.z,
+                    frequency = hses.f$mpd.obs.z,
+                    sample.pool = hses.sp$mpd.obs.z,
+                    phylogeny.pool = hses.pp$mpd.obs.z,
+                    independentswap = hses.is$mpd.obs.z,
+                    trialswap = hses.ts$mpd.obs.z,
+                    taxa.labels.a = hses.a.tl$mpd.obs.z, 
+                    richness.a = hses.a.r$mpd.obs.z,
+                    frequency.a = hses.a.f$mpd.obs.z,
+                    sample.pool.a = hses.a.sp$mpd.obs.z,
+                    phylogeny.pool.a = hses.a.pp$mpd.obs.z,
+                    independentswap.a = hses.a.is$mpd.obs.z,
+                    trialswap.a = hses.a.ts$mpd.obs.z)
+
+hses.tl.m <- lm(taxa.labels ~ n, h.ses)
+plot(h.ses$n, h.ses$taxa.labels)
+abline(hses.tl.m)
+
+shapiro.test(resid(hses.tl.m))
+library(nortest)
+lillie.test(resid(hses.tl.m))
+ad.test(resid(hses.tl.m))
+
+#_____________________________________________________________________
+
+library(car)
+ncvTest(hses.tl.m)
+
+cor(h.ses$n, h.ses$taxa.labels)
+cor.test(h.ses$n, h.ses$taxa.labels)
+
+cor(h.ses$n, h.ses$taxa.labels, method = "spearman")
+cor.test(h.ses$n, h.ses$taxa.labels, method = "spearman")
+
+cor(h.ses$n, h.ses$taxa.labels, method = "kendall")
+cor.test(h.ses$n, h.ses$taxa.labels, method = "kendall")
+
+save.image("herb-phylo-analysis.RData")
+
--- tree-shrub-phylo-analysis.R
+++ tree-shrub-phylo-analysis.R
@@ -3,24 +3,148 @@ load('tree-shrub-phylo.RData')
 library(ape)
 dist.mat <- cophenetic(tree)              
 library(picante)
-mpd.mat <- mpd(ta, cophenetic(tree), abundance.weighted = F) #NA
-smpd.mat <- mpd(sa, cophenetic(tree), abundance.weighted = F)
+mpd.mat <- mpd(ta, dist.mat, abundance.weighted = F)
+smpd.mat <- mpd(sa, dist.mat, abundance.weighted = F)
 
 #_____________________________________________________________________
 
-mpd.mat.a <- mpd(ta, cophenetic(tree), abundance.weighted = T)
-smpd.mat.a <- mpd(sa, cophenetic(tree), abundance.weighted = T)
+mpd.mat.a <- mpd(ta, dist.mat, abundance.weighted = T)
+smpd.mat.a <- mpd(sa, dist.mat, abundance.weighted = T)
 
 #_____________________________________________________________________
 
-ses <- ses.mpd(ta, dist.mat, null.model = "taxa.labels", runs = 999, iterations = 1000)
+tses.tl <- ses.mpd(ta, dist.mat, null.model = "taxa.labels", runs = 999, iterations = 1000)
 
-plot(1:96, ses$mpd.obs)
-plot(1:96, ses$mpd.obs.z)
-plot(1:96, ses$mpd.obs.p)
+tses.r <- ses.mpd(ta, dist.mat, null.model = "richness", runs = 999, iterations = 1000)
 
-ses.a <- ses.mpd(sa, dist.mat, null.model = "taxa.labels", runs = 999, iterations = 1000)
-plot(1:96, ses$mpd.obs)
-plot(1:96, ses$mpd.obs.z)
-plot(1:96, ses$mpd.obs.p)
+tses.f <- ses.mpd(ta, dist.mat, null.model = "frequency", runs = 999, iterations = 1000)
 
+tses.sp <- ses.mpd(ta, dist.mat, null.model = "sample.pool", runs = 999, iterations = 1000)
+
+tses.pp <- ses.mpd(ta, dist.mat, null.model = "phylogeny.pool", runs = 999, iterations = 1000)
+
+tses.is <- ses.mpd(ta, dist.mat, null.model = "independentswap", runs = 999, iterations = 1000)
+
+tses.ts <- ses.mpd(ta, dist.mat, null.model = "trialswap", runs = 999, iterations = 1000)
+
+#_____________________________________________________________________
+
+tses.a.tl <- ses.mpd(ta, dist.mat, null.model = "taxa.labels", runs = 999, iterations = 1000, abundance.weighted = T)
+
+tses.a.r <- ses.mpd(ta, dist.mat, null.model = "richness", runs = 999, iterations = 1000, abundance.weighted = T)
+
+tses.a.f <- ses.mpd(ta, dist.mat, null.model = "frequency", runs = 999, iterations = 1000, abundance.weighted = T)
+
+tses.a.sp <- ses.mpd(ta, dist.mat, null.model = "sample.pool", runs = 999, iterations = 1000, abundance.weighted = T)
+
+tses.a.pp <- ses.mpd(ta, dist.mat, null.model = "phylogeny.pool", runs = 999, iterations = 1000, abundance.weighted = T)
+
+tses.a.is <- ses.mpd(ta, dist.mat, null.model = "independentswap", runs = 999, iterations = 1000, abundance.weighted = T)
+
+tses.a.ts  <- ses.mpd(ta, dist.mat, null.model = "trialswap", runs = 999, iterations = 1000, abundance.weighted = T)
+
+#_____________________________________________________________________
+
+sses.tl <- ses.mpd(sa, dist.mat, null.model = "taxa.labels", runs = 999, iterations = 1000)
+
+sses.r <- ses.mpd(sa, dist.mat, null.model = "richness", runs = 999, iterations = 1000)
+
+sses.f <- ses.mpd(sa, dist.mat, null.model = "frequency", runs = 999, iterations = 1000)
+
+sses.sp <- ses.mpd(sa, dist.mat, null.model = "sample.pool", runs = 999, iterations = 1000)
+
+sses.pp <- ses.mpd(sa, dist.mat, null.model = "phylogeny.pool", runs = 999, iterations = 1000)
+
+sses.is <- ses.mpd(sa, dist.mat, null.model = "independentswap", runs = 999, iterations = 1000)
+
+sses.ts <- ses.mpd(sa, dist.mat, null.model = "trialswap", runs = 999, iterations = 1000)
+
+#_____________________________________________________________________
+
+sses.a.tl <- ses.mpd(sa, dist.mat, null.model = "taxa.labels", runs = 999, iterations = 1000, abundance.weighted = T)
+
+sses.a.r <- ses.mpd(sa, dist.mat, null.model = "richness", runs = 999, iterations = 1000, abundance.weighted = T)
+
+sses.a.f <- ses.mpd(sa, dist.mat, null.model = "frequency", runs = 999, iterations = 1000, abundance.weighted = T)
+
+sses.a.sp <- ses.mpd(sa, dist.mat, null.model = "sample.pool", runs = 999, iterations = 1000, abundance.weighted = T)
+
+sses.a.pp <- ses.mpd(sa, dist.mat, null.model = "phylogeny.pool", runs = 999, iterations = 1000, abundance.weighted = T)
+
+sses.a.is <- ses.mpd(sa, dist.mat, null.model = "independentswap", runs = 999, iterations = 1000, abundance.weighted = T)
+
+sses.a.ts <- ses.mpd(sa, dist.mat, null.model = "trialswap", runs = 999, iterations = 1000, abundance.weighted = T)
+
+rm(mpd.mat, mpd.mat.a, smpd.mat, smpd.mat.a, my.add, my.bind.tip)
+save.image("tree-shrub-phylo-analysis-ses.RData")
+
+#_____________________________________________________________________
+
+plot(1:96, tses.tl$mpd.obs)
+plot(1:96, tses.tl$mpd.obs.z)
+
+t.ses <- data.frame(n = 1:96, taxa.labels = tses.tl$mpd.obs.z,
+                richness = tses.r$mpd.obs.z,
+                frequency = tses.f$mpd.obs.z,
+                sample.pool = tses.sp$mpd.obs.z,
+                phylogeny.pool = tses.pp$mpd.obs.z,
+                independentswap = tses.is$mpd.obs.z,
+                trialswap = tses.ts$mpd.obs.z,
+                taxa.labels.a = tses.a.tl$mpd.obs.z, 
+                richness.a = tses.a.r$mpd.obs.z,
+                frequency.a = tses.a.f$mpd.obs.z,
+                sample.pool.a = tses.a.sp$mpd.obs.z,
+                phylogeny.pool.a = tses.a.pp$mpd.obs.z,
+                independentswap.a = tses.a.is$mpd.obs.z,
+                trialswap.a = tses.a.ts$mpd.obs.z)
+
+s.ses <- data.frame(n = 1:96, taxa.labels = sses.tl$mpd.obs.z,
+                    richness = sses.r$mpd.obs.z,
+                    frequency = sses.f$mpd.obs.z,
+                    sample.pool = sses.sp$mpd.obs.z,
+                    phylogeny.pool = sses.pp$mpd.obs.z,
+                    independentswap = sses.is$mpd.obs.z,
+                    trialswap = sses.ts$mpd.obs.z,
+                    taxa.labels.a = sses.a.tl$mpd.obs.z, 
+                    richness.a = sses.a.r$mpd.obs.z,
+                    frequency.a = sses.a.f$mpd.obs.z,
+                    sample.pool.a = sses.a.sp$mpd.obs.z,
+                    phylogeny.pool.a = sses.a.pp$mpd.obs.z,
+                    independentswap.a = sses.a.is$mpd.obs.z,
+                    trialswap.a = sses.a.ts$mpd.obs.z)
+
+tses.tl.m <- lm(taxa.labels ~ n, t.ses)
+plot(t.ses$n, t.ses$taxa.labels)
+abline(tses.tl.m)
+
+shapiro.test(resid(tses.tl.m))
+library(nortest)
+lillie.test(resid(tses.tl.m))
+ad.test(resid(tses.tl.m))
+
+library(car)
+ncvTest(tses.tl.m)
+
+#_____________________________________________________________________
+
+cor(t.ses$n[!is.na(t.ses$taxa.labels)], t.ses$taxa.labels[!is.na(t.ses$taxa.labels)])
+cor.test(t.ses$n[!is.na(t.ses$taxa.labels)], t.ses$taxa.labels[!is.na(t.ses$taxa.labels)])
+
+cor(t.ses$n[!is.na(t.ses$taxa.labels)], t.ses$taxa.labels[!is.na(t.ses$taxa.labels)], method = "spearman")
+cor.test(t.ses$n[!is.na(t.ses$taxa.labels)], t.ses$taxa.labels[!is.na(t.ses$taxa.labels)], method = "spearman")
+
+cor(t.ses$n[!is.na(t.ses$taxa.labels)], t.ses$taxa.labels[!is.na(t.ses$taxa.labels)], method = "kendall")
+cor.test(t.ses$n[!is.na(t.ses$taxa.labels)], t.ses$taxa.labels[!is.na(t.ses$taxa.labels)], method = "kendall")
+
+#_____________________________________________________________________
+
+cor(s.ses$n, s.ses$taxa.labels)
+cor.test(s.ses$n, s.ses$taxa.labels)
+
+cor(s.ses$n, s.ses$taxa.labels, method = "spearman")
+cor.test(s.ses$n, s.ses$taxa.labels, method = "spearman")
+
+cor(s.ses$n, s.ses$taxa.labels, method = "kendall")
+cor.test(s.ses$n, s.ses$taxa.labels, method = "kendall")
+
+save.image("tree-shrub-phylo-analysis.RData")
