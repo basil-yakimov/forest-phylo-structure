94988042424f767f6410329b075ce99c9984989f
basil-yakimov <damselfly@yandex.ru>
2019-02-15 15:36:49 +0300

    new scaling draft

--- scaling.R
+++ scaling.R
@@ -4,6 +4,35 @@ load("clean.data/herb-abund.rda")
 
 hgt <- read.table("raw.data/hgt.txt")[[1]]
 
+
+pairs <- matrix(c(11, 12, 56, 57), ncol = 2, byrow = T)
+
+sc <- 2:15
+
+out <- vector(mode = "list", length = length(sc))
+
+for (jj in 1:length(sc))
+{
+  dd <- matrix(0, nrow = 96-sc[jj]+1, ncol = 149)
+  del <- NULL
+  for (ii in 1:(96-sc[jj]+1))
+  {
+    dd[ii, ] <- colSums(ha[(ii):(ii+sc[jj]-1), ])
+    for (uu in 1:nrow(pairs))
+    {
+      if ( pairs[uu, 1]  %in% ((ii):(ii+sc[jj]-1)) & pairs[uu, 2] %in% (ii):(ii+sc[jj]-1) )
+      {
+        del <- c(del, ii)
+      }
+    }
+  }
+  
+  del <- unique(del)
+  
+  out[[jj]] <- dd[-del, ]
+}
+
+
 #________________________________________
 
 #1110-1180,1190-1255, 1260-1338, 1344-1408, 1414-1496, 1504-1552, 1558-1616, 1622-1682, and 1687-1734
