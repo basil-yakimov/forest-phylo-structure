2ea5f2d21f936ef74de79fae78adf8792965b8e8
Alina Gerasimova <43878627+arrirh@users.noreply.github.com>
2019-02-24 19:14:39 +0300

    new scaling (#8)
    * minor changes
    
    * new scaling draft
    
    * new scaling

--- scaling-2.phylogenetic-analysis.r
+++ scaling-2.phylogenetic-analysis.r
@@ -7,173 +7,55 @@ load("clean.data/herb-phylo.rda")
 wood.mat <- cophenetic(wood.tree)
 herb.mat <- cophenetic(herb.tree)
 
-load("clean.data/scaling-2-abund.rda")
-
-#---tree layer analysis ignoring abundances with various null models---#
-
-t.ses.tl <- ses.mpd(ta, wood.mat, null.model = "taxa.labels", runs = 999, iterations = 1000)
-t.ses.r  <- ses.mpd(ta, wood.mat, null.model = "richness", runs = 999, iterations = 1000)
-t.ses.f  <- ses.mpd(ta, wood.mat, null.model = "frequency", runs = 999, iterations = 1000)
-t.ses.sp <- ses.mpd(ta, wood.mat, null.model = "sample.pool", runs = 999, iterations = 1000)
-t.ses.pp <- ses.mpd(ta, wood.mat, null.model = "phylogeny.pool", runs = 999, iterations = 1000)
-t.ses.is <- ses.mpd(ta, wood.mat, null.model = "independentswap", runs = 999, iterations = 1000)
-t.ses.ts <- ses.mpd(ta, wood.mat, null.model = "trialswap", runs = 999, iterations = 1000)
-
-#---tree layer analysis considering abundances with various null models---#
-
-t.ses.a.tl <- ses.mpd(ta, wood.mat, null.model = "taxa.labels", runs = 999, iterations = 1000, abundance.weighted = T)
-t.ses.a.r  <- ses.mpd(ta, wood.mat, null.model = "richness", runs = 999, iterations = 1000, abundance.weighted = T)
-t.ses.a.f  <- ses.mpd(ta, wood.mat, null.model = "frequency", runs = 999, iterations = 1000, abundance.weighted = T)
-t.ses.a.sp <- ses.mpd(ta, wood.mat, null.model = "sample.pool", runs = 999, iterations = 1000, abundance.weighted = T)
-t.ses.a.pp <- ses.mpd(ta, wood.mat, null.model = "phylogeny.pool", runs = 999, iterations = 1000, abundance.weighted = T)
-t.ses.a.is <- ses.mpd(ta, wood.mat, null.model = "independentswap", runs = 999, iterations = 1000, abundance.weighted = T)
-t.ses.a.ts <- ses.mpd(ta, wood.mat, null.model = "trialswap", runs = 999, iterations = 1000, abundance.weighted = T)
-
-#---shrub layer analysis ignoring abundances with various null models---#
-
-s.ses.tl <- ses.mpd(sa, wood.mat, null.model = "taxa.labels", runs = 999, iterations = 1000)
-s.ses.r  <- ses.mpd(sa, wood.mat, null.model = "richness", runs = 999, iterations = 1000)
-s.ses.f  <- ses.mpd(sa, wood.mat, null.model = "frequency", runs = 999, iterations = 1000)
-s.ses.sp <- ses.mpd(sa, wood.mat, null.model = "sample.pool", runs = 999, iterations = 1000)
-s.ses.pp <- ses.mpd(sa, wood.mat, null.model = "phylogeny.pool", runs = 999, iterations = 1000)
-s.ses.is <- ses.mpd(sa, wood.mat, null.model = "independentswap", runs = 999, iterations = 1000)
-s.ses.ts <- ses.mpd(sa, wood.mat, null.model = "trialswap", runs = 999, iterations = 1000)
-
-#---shrub layer analysis considering abundances with various null models---#
-
-s.ses.a.tl <- ses.mpd(sa, wood.mat, null.model = "taxa.labels", runs = 999, iterations = 1000, abundance.weighted = T)
-s.ses.a.r  <- ses.mpd(sa, wood.mat, null.model = "richness", runs = 999, iterations = 1000, abundance.weighted = T)
-s.ses.a.f  <- ses.mpd(sa, wood.mat, null.model = "frequency", runs = 999, iterations = 1000, abundance.weighted = T)
-s.ses.a.sp <- ses.mpd(sa, wood.mat, null.model = "sample.pool", runs = 999, iterations = 1000, abundance.weighted = T)
-s.ses.a.pp <- ses.mpd(sa, wood.mat, null.model = "phylogeny.pool", runs = 999, iterations = 1000, abundance.weighted = T)
-s.ses.a.is <- ses.mpd(sa, wood.mat, null.model = "independentswap", runs = 999, iterations = 1000, abundance.weighted = T)
-s.ses.a.ts <- ses.mpd(sa, wood.mat, null.model = "trialswap", runs = 999, iterations = 1000, abundance.weighted = T)
-
-#---herb layer analysis ignoring abundances with various null models---#
-
-h.ses.tl <- ses.mpd(ha, herb.mat, null.model = "taxa.labels", runs = 999, iterations = 1000)
-h.ses.r  <- ses.mpd(ha, herb.mat, null.model = "richness", runs = 999, iterations = 1000)
-h.ses.f  <- ses.mpd(ha, herb.mat, null.model = "frequency", runs = 999, iterations = 1000)
-h.ses.sp <- ses.mpd(ha, herb.mat, null.model = "sample.pool", runs = 999, iterations = 1000)
-h.ses.pp <- ses.mpd(ha, herb.mat, null.model = "phylogeny.pool", runs = 999, iterations = 1000)
-h.ses.is <- ses.mpd(ha, herb.mat, null.model = "independentswap", runs = 999, iterations = 1000)
-h.ses.ts <- ses.mpd(ha, herb.mat, null.model = "trialswap", runs = 999, iterations = 1000)
-
-#---herb layer analysis considering abundances with various null models---#
-
-h.ses.a.tl <- ses.mpd(ha, herb.mat, null.model = "taxa.labels", runs = 999, iterations = 1000, abundance.weighted = T)
-h.ses.a.r  <- ses.mpd(ha, herb.mat, null.model = "richness", runs = 999, iterations = 1000, abundance.weighted = T)
-h.ses.a.f  <- ses.mpd(ha, herb.mat, null.model = "frequency", runs = 999, iterations = 1000, abundance.weighted = T)
-h.ses.a.sp <- ses.mpd(ha, herb.mat, null.model = "sample.pool", runs = 999, iterations = 1000, abundance.weighted = T)
-h.ses.a.pp <- ses.mpd(ha, herb.mat, null.model = "phylogeny.pool", runs = 999, iterations = 1000, abundance.weighted = T)
-h.ses.a.is <- ses.mpd(ha, herb.mat, null.model = "independentswap", runs = 999, iterations = 1000, abundance.weighted = T)
-h.ses.a.ts <- ses.mpd(ha, herb.mat, null.model = "trialswap", runs = 999, iterations = 1000, abundance.weighted = T)
+load("clean.data/scaling-abund.rda")
 
 
-#--- re-pack and save results ---#
+#________________________________________________
 
-hgt <- read.table("raw.data/hgt.txt")[[1]]
-count <- c(2:11, 13:20, 22:33, 35:44, 46:58, 60:66, 68:76, 78:87, 89:96)
-hgt2 <- c()
-for (ii in count){
-  hgt2 <- c(hgt2, mean(hgt[ii-1], hgt[ii]))
+c <- vector(mode = "list", length = 3)
+for (ii in 1:3){
+  
+  g <- vector(mode = "list", length = 7)
+  
+  for (jj in 1:7){
+    df <- out1_tot[[ii]][[jj]]
+    
+    if (ii == 3){
+      ses <- ses.mpd(df, herb.mat, null.model = "richness", runs = 999, iterations = 1000)
+    } else {
+      ses <- ses.mpd(df, wood.mat, null.model = "richness", runs = 999, iterations = 1000)
+    }
+    m <- cbind(ses$mpd.obs.z, ses$mpd.obs.p)
+    colnames(m) <- c("mpd.obs.z", "mpd.obs.p")
+    g[[jj]] <- m
+    
+  }
+  
+  c[[ii]] <- g
 }
-hgt <- hgt2
 
-t.ses.z2 <- data.frame(hgt = hgt, 
-                      taxa.labels = t.ses.tl$mpd.obs.z,
-                      richness = t.ses.r$mpd.obs.z,
-                      frequency = t.ses.f$mpd.obs.z,
-                      sample.pool = t.ses.sp$mpd.obs.z,
-                      phylogeny.pool = t.ses.pp$mpd.obs.z,
-                      independentswap = t.ses.is$mpd.obs.z,
-                      trialswap = t.ses.ts$mpd.obs.z,
-                      taxa.labels.a = t.ses.a.tl$mpd.obs.z, 
-                      richness.a = t.ses.a.r$mpd.obs.z,
-                      frequency.a = t.ses.a.f$mpd.obs.z,
-                      sample.pool.a = t.ses.a.sp$mpd.obs.z,
-                      phylogeny.pool.a = t.ses.a.pp$mpd.obs.z,
-                      independentswap.a = t.ses.a.is$mpd.obs.z,
-                      trialswap.a = t.ses.a.ts$mpd.obs.z)
+#________________________________________________
 
-t.ses.p2 <- data.frame(hgt = hgt, 
-                      taxa.labels = t.ses.tl$mpd.obs.p,
-                      richness = t.ses.r$mpd.obs.p,
-                      frequency = t.ses.f$mpd.obs.p,
-                      sample.pool = t.ses.sp$mpd.obs.p,
-                      phylogeny.pool = t.ses.pp$mpd.obs.p,
-                      independentswap = t.ses.is$mpd.obs.p,
-                      trialswap = t.ses.ts$mpd.obs.p,
-                      taxa.labels.a = t.ses.a.tl$mpd.obs.p, 
-                      richness.a = t.ses.a.r$mpd.obs.p,
-                      frequency.a = t.ses.a.f$mpd.obs.p,
-                      sample.pool.a = t.ses.a.sp$mpd.obs.p,
-                      phylogeny.pool.a = t.ses.a.pp$mpd.obs.p,
-                      independentswap.a = t.ses.a.is$mpd.obs.p,
-                      trialswap.a = t.ses.a.ts$mpd.obs.p)
+c2 <- vector(mode = "list", length = 3)
+for (ii in 1:3){
+  
+  g <- vector(mode = "list", length = 95)
+  
+  for (jj in 1:7){
+    df <- out1_tot[[ii]][[jj]]
+    
+    if (ii == 3){
+      ses <- ses.mpd(df, herb.mat, null.model = "richness", runs = 999, iterations = 1000)
+    } else {
+      ses <- ses.mpd(df, wood.mat, null.model = "richness", runs = 999, iterations = 1000)
+    }
+    m <- cbind(ses$mpd.obs.z, ses$mpd.obs.p)
+    colnames(m) <- c("mpd.obs.z", "mpd.obs.p")
+    g[[jj]] <- m
+    
+  }
+  
+  c2[[ii]] <- g
+}
 
-s.ses.z2 <- data.frame(hgt = hgt, 
-                      taxa.labels = s.ses.tl$mpd.obs.z,
-                      richness = s.ses.r$mpd.obs.z,
-                      frequency = s.ses.f$mpd.obs.z,
-                      sample.pool = s.ses.sp$mpd.obs.z,
-                      phylogeny.pool = s.ses.pp$mpd.obs.z,
-                      independentswap = s.ses.is$mpd.obs.z,
-                      trialswap = s.ses.ts$mpd.obs.z,
-                      taxa.labels.a = s.ses.a.tl$mpd.obs.z, 
-                      richness.a = s.ses.a.r$mpd.obs.z,
-                      frequency.a = s.ses.a.f$mpd.obs.z,
-                      sample.pool.a = s.ses.a.sp$mpd.obs.z,
-                      phylogeny.pool.a = s.ses.a.pp$mpd.obs.z,
-                      independentswap.a = s.ses.a.is$mpd.obs.z,
-                      trialswap.a = s.ses.a.ts$mpd.obs.z)
-
-s.ses.p2 <- data.frame(hgt = hgt, 
-                      taxa.labels = s.ses.tl$mpd.obs.p,
-                      richness = s.ses.r$mpd.obs.p,
-                      frequency = s.ses.f$mpd.obs.p,
-                      sample.pool = s.ses.sp$mpd.obs.p,
-                      phylogeny.pool = s.ses.pp$mpd.obs.p,
-                      independentswap = s.ses.is$mpd.obs.p,
-                      trialswap = s.ses.ts$mpd.obs.p,
-                      taxa.labels.a = s.ses.a.tl$mpd.obs.p, 
-                      richness.a = s.ses.a.r$mpd.obs.p,
-                      frequency.a = s.ses.a.f$mpd.obs.p,
-                      sample.pool.a = s.ses.a.sp$mpd.obs.p,
-                      phylogeny.pool.a = s.ses.a.pp$mpd.obs.p,
-                      independentswap.a = s.ses.a.is$mpd.obs.p,
-                      trialswap.a = s.ses.a.ts$mpd.obs.p)
-
-h.ses.z2 <- data.frame(hgt = hgt, 
-                      taxa.labels = h.ses.tl$mpd.obs.z,
-                      richness = h.ses.r$mpd.obs.z,
-                      frequency = h.ses.f$mpd.obs.z,
-                      sample.pool = h.ses.sp$mpd.obs.z,
-                      phylogeny.pool = h.ses.pp$mpd.obs.z,
-                      independentswap = h.ses.is$mpd.obs.z,
-                      trialswap = h.ses.ts$mpd.obs.z,
-                      taxa.labels.a = h.ses.a.tl$mpd.obs.z, 
-                      richness.a = h.ses.a.r$mpd.obs.z,
-                      frequency.a = h.ses.a.f$mpd.obs.z,
-                      sample.pool.a = h.ses.a.sp$mpd.obs.z,
-                      phylogeny.pool.a = h.ses.a.pp$mpd.obs.z,
-                      independentswap.a = h.ses.a.is$mpd.obs.z,
-                      trialswap.a = h.ses.a.ts$mpd.obs.z)
-
-h.ses.p2 <- data.frame(hgt = hgt, 
-                      taxa.labels = h.ses.tl$mpd.obs.p,
-                      richness = h.ses.r$mpd.obs.p,
-                      frequency = h.ses.f$mpd.obs.p,
-                      sample.pool = h.ses.sp$mpd.obs.p,
-                      phylogeny.pool = h.ses.pp$mpd.obs.p,
-                      independentswap = h.ses.is$mpd.obs.p,
-                      trialswap = h.ses.ts$mpd.obs.p,
-                      taxa.labels.a = h.ses.a.tl$mpd.obs.p, 
-                      richness.a = h.ses.a.r$mpd.obs.p,
-                      frequency.a = h.ses.a.f$mpd.obs.p,
-                      sample.pool.a = h.ses.a.sp$mpd.obs.p,
-                      phylogeny.pool.a = h.ses.a.pp$mpd.obs.p,
-                      independentswap.a = h.ses.a.is$mpd.obs.p,
-                      trialswap.a = h.ses.a.ts$mpd.obs.p)
-
-save(t.ses.z2, t.ses.p2, s.ses.z2, s.ses.p2, h.ses.z2, h.ses.p2, file = "clean.data/scaling-2-phylo-ses.rda")
+save(c, c2, file = "clean.data/scaling-phylo-ses.rda")
--- scaling.R
+++ scaling.R
@@ -5,6 +5,12 @@ load("clean.data/herb-abund.rda")
 hgt <- read.table("raw.data/hgt.txt")[[1]]
 
 
+a <- vector(mode = "list", length = 3)
+a[[1]] <- ta
+a[[2]] <- sa
+a[[3]] <- ha
+
+
 pairs <- matrix(c(11, 12, 56, 57), ncol = 2, byrow = T)
 
 sc <- 2:15
@@ -33,9 +39,18 @@ for (jj in 1:length(sc))
 }
 
 
+
 #________________________________________
 
+#considering the boundaries between transects
+
 #1110-1180,1190-1255, 1260-1338, 1344-1408, 1414-1496, 1504-1552, 1558-1616, 1622-1682, and 1687-1734
+
+#hgt <- read.table("raw.data/hgt.txt")[[1]]
+#h <- row.names(ta)
+#names(h) <- hgt
+pairs <- matrix(c(11, 12, 20, 21, 33, 34, 44, 45, 58, 59, 66, 67, 76, 77, 87, 88), ncol = 2, byrow = T)
+
 h <- row.names(ta)
 names(h) <- hgt #looking on the numbers of sites
 count <- c(2:11, 13:20, 22:33, 35:44, 46:58, 60:66, 68:76, 78:87, 89:96)
@@ -49,32 +64,89 @@ for (ii in count){
 }
 ta2 <- ta2[count, ] #removing residues
 
-#________________________________________
 
-sa2 <- sa
-for (ii in count){
-  a <- sa[ii-1, ] + sa[ii, ]
-  for (i in 1:length(a)){
-    sa2[ii, i] <- a[i]
+sc1 <- 2:8
+
+out1_tot <- vector(mode = "list", length = 3)
+
+for (i in 1:3){
+  dt <- a[[i]]
+  out <- vector(mode = "list", length = length(sc1))
+  hgt_sc <- vector(mode = "list", length = 7) 
+  
+  for (jj in 1:length(sc1))
+  {
+    dd <- matrix(0, nrow = 96-sc1[jj]+1, ncol = ncol(dt)) 
+    del <- NULL 
+    h <- NULL
+    
+    for (ii in 1:(96-sc1[jj]+1))
+    {
+      dd[ii, ] <- colSums(dt[(ii):(ii+sc1[jj]-1), ]) 
+      h <- c(h, mean(hgt[(ii):(ii+sc1[jj]-1)]))
+      
+      for (uu in 1:nrow(pairs))
+      {
+        if ( pairs[uu, 1]  %in% ((ii):(ii+sc1[jj]-1)) & pairs[uu, 2] %in% (ii):(ii+sc1[jj]-1) )
+        {
+          del <- c(del, ii)
+        }
+      }
+    }
+    
+    del <- unique(del)
+    h <- h[-del]
+    hgt_sc[[jj]] <- h
+    
+    colnames(dd) <- colnames(dt)
+    out[[jj]] <- dd[-del, ] 
+    
   }
+  
+  out1_tot[[i]] <- out
 }
-sa2 <- sa2[count, ]
+
+
 
 #________________________________________
 
-ha2 <- ha
-for (ii in count){
-  a <- ha[ii-1, ] + ha[ii, ]
-  for (i in 1:length(a)){
-    ha2[ii, i] <- a[i]
+#ignoring the boundaries between transects
+
+
+out2_tot <- vector(mode = "list", length = 3)
+
+for (i in 1:3){
+  dt <- a[[i]]
+  
+  sc <- 2:nrow(dt)
+  
+  out2 <- vector(mode = "list", length = length(sc))
+  hgt_sc2 <- vector(mode = "list", length = 7) 
+  
+  for (jj in 1:length(sc))
+  {
+    dd <- matrix(0, nrow = 96-sc[jj]+1, ncol = ncol(dt))
+    h <- NULL
+    for (ii in 1:(96-sc[jj]+1))
+    {
+      dd[ii, ] <- colSums(dt[(ii):(ii+sc[jj]-1), ])
+      h <- c(h, mean(hgt[(ii):(ii+sc[jj]-1)]))
+    }
+    
+    hgt_sc2[[jj]] <- h
+    
+    colnames(dd) <- colnames(dt)
+    out2[[jj]] <- dd
   }
+  
+  out2_tot[[i]] <- out2
 }
-ha2 <- ha2[count, ]
+
+
+
 
 #________________________________________
 
-ha <- ha2
-sa <- sa2
-ta <- ta2
 
-save(ta, sa, ha, file = "clean.data/scaling-2-abund.rda")
+save(out1_tot, out2_tot, file = "clean.data/scaling-abund.rda")
+save(hgt_sc, hgt_sc2, file = "clean.data/scaling-hgt.rda")
