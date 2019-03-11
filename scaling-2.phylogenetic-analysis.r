e31c0ab32d87115e826a3bd0074c8bf2d522c288
arrirh <arrirh@gmail.com>
2019-03-11 23:51:40 +0300

    new scaling [2]

--- scaling-2.phylogenetic-analysis.r
+++ scaling-2.phylogenetic-analysis.r
@@ -12,13 +12,13 @@ load("clean.data/scaling-abund.rda")
 
 #________________________________________________
 
-c <- vector(mode = "list", length = 3)
+s <- vector(mode = "list", length = 3)
 for (ii in 1:3){
   
-  g <- vector(mode = "list", length = 7)
+  g <- vector(mode = "list", length = 20)
   
-  for (jj in 1:7){
-    df <- out1_tot[[ii]][[jj]]
+  for (jj in 1:20){
+    df <- out_tot[[ii]][[jj]]
     
     if (ii == 3){
       ses <- ses.mpd(df, herb.mat, null.model = "richness", runs = 999, iterations = 1000)
@@ -31,31 +31,7 @@ for (ii in 1:3){
     
   }
   
-  c[[ii]] <- g
+  s[[ii]] <- g
 }
 
-#________________________________________________
-
-c2 <- vector(mode = "list", length = 3)
-for (ii in 1:3){
-  
-  g <- vector(mode = "list", length = 95)
-  
-  for (jj in 1:7){
-    df <- out1_tot[[ii]][[jj]]
-    
-    if (ii == 3){
-      ses <- ses.mpd(df, herb.mat, null.model = "richness", runs = 999, iterations = 1000)
-    } else {
-      ses <- ses.mpd(df, wood.mat, null.model = "richness", runs = 999, iterations = 1000)
-    }
-    m <- cbind(ses$mpd.obs.z, ses$mpd.obs.p)
-    colnames(m) <- c("mpd.obs.z", "mpd.obs.p")
-    g[[jj]] <- m
-    
-  }
-  
-  c2[[ii]] <- g
-}
-
-save(c, c2, file = "clean.data/scaling-phylo-ses.rda")
+save(s, file = "clean.data/scaling-phylo-ses.rda")
--- scaling.R
+++ scaling.R
@@ -11,142 +11,51 @@ a[[2]] <- sa
 a[[3]] <- ha
 
 
-pairs <- matrix(c(11, 12, 56, 57), ncol = 2, byrow = T)
-
-sc <- 2:15
-
-out <- vector(mode = "list", length = length(sc))
-
-for (jj in 1:length(sc))
-{
-  dd <- matrix(0, nrow = 96-sc[jj]+1, ncol = 149)
-  del <- NULL
-  for (ii in 1:(96-sc[jj]+1))
-  {
-    dd[ii, ] <- colSums(ha[(ii):(ii+sc[jj]-1), ])
-    for (uu in 1:nrow(pairs))
-    {
-      if ( pairs[uu, 1]  %in% ((ii):(ii+sc[jj]-1)) & pairs[uu, 2] %in% (ii):(ii+sc[jj]-1) )
-      {
-        del <- c(del, ii)
-      }
-    }
-  }
-  
-  del <- unique(del)
-  
-  out[[jj]] <- dd[-del, ]
-}
-
-
-
 #________________________________________
 
 #considering the boundaries between transects
 
 #1110-1180,1190-1255, 1260-1338, 1344-1408, 1414-1496, 1504-1552, 1558-1616, 1622-1682, and 1687-1734
 
-#hgt <- read.table("raw.data/hgt.txt")[[1]]
-#h <- row.names(ta)
-#names(h) <- hgt
-pairs <- matrix(c(11, 12, 20, 21, 33, 34, 44, 45, 58, 59, 66, 67, 76, 77, 87, 88), ncol = 2, byrow = T)
-
-h <- row.names(ta)
-names(h) <- hgt #looking on the numbers of sites
-count <- c(2:11, 13:20, 22:33, 35:44, 46:58, 60:66, 68:76, 78:87, 89:96)
-
-ta2 <- ta
-for (ii in count){
-  a <- ta[ii-1, ] + ta[ii, ]
-  for (i in 1:length(a)){   #filling the row
-    ta2[ii, i] <- a[i]
-  }
-}
-ta2 <- ta2[count, ] #removing residues
-
-
-sc1 <- 2:8
-
-out1_tot <- vector(mode = "list", length = 3)
-
-for (i in 1:3){
-  dt <- a[[i]]
-  out <- vector(mode = "list", length = length(sc1))
-  hgt_sc <- vector(mode = "list", length = 7) 
-  
-  for (jj in 1:length(sc1))
-  {
-    dd <- matrix(0, nrow = 96-sc1[jj]+1, ncol = ncol(dt)) 
-    del <- NULL 
-    h <- NULL
-    
-    for (ii in 1:(96-sc1[jj]+1))
-    {
-      dd[ii, ] <- colSums(dt[(ii):(ii+sc1[jj]-1), ]) 
-      h <- c(h, mean(hgt[(ii):(ii+sc1[jj]-1)]))
-      
-      for (uu in 1:nrow(pairs))
-      {
-        if ( pairs[uu, 1]  %in% ((ii):(ii+sc1[jj]-1)) & pairs[uu, 2] %in% (ii):(ii+sc1[jj]-1) )
-        {
-          del <- c(del, ii)
-        }
-      }
-    }
-    
-    del <- unique(del)
-    h <- h[-del]
-    hgt_sc[[jj]] <- h
-    
-    colnames(dd) <- colnames(dt)
-    out[[jj]] <- dd[-del, ] 
-    
-  }
-  
-  out1_tot[[i]] <- out
-}
-
-
+#pairs <- matrix(c(11, 12, 20, 21, 33, 34, 44, 45, 58, 59, 66, 67, 76, 77, 87, 88), ncol = 2, byrow = T)
 
 #________________________________________
 
-#ignoring the boundaries between transects
-
-
-out2_tot <- vector(mode = "list", length = 3)
+out_tot <- vector(mode = "list", length = 3)
 
 for (i in 1:3){
   dt <- a[[i]]
   
-  sc <- 2:nrow(dt)
+  sc <- 1:20
   
   out2 <- vector(mode = "list", length = length(sc))
-  hgt_sc2 <- vector(mode = "list", length = 7) 
+  hgt_sc <- vector(mode = "list", length = 7) 
   
   for (jj in 1:length(sc))
   {
-    dd <- matrix(0, nrow = 96-sc[jj]+1, ncol = ncol(dt))
+    dd <- matrix(0, nrow = (96-sc[jj]+1), ncol = ncol(dt))
     h <- NULL
+    del <- NULL
     for (ii in 1:(96-sc[jj]+1))
     {
       dd[ii, ] <- colSums(dt[(ii):(ii+sc[jj]-1), ])
+      del <- c(del, ii %in% seq(1, 96, by = sc[jj]))
+    }
+    
+    for (ii in seq(1, 96-sc[jj]+1, by = sc[jj])){
       h <- c(h, mean(hgt[(ii):(ii+sc[jj]-1)]))
     }
     
-    hgt_sc2[[jj]] <- h
+    dd <- dd[del, ]
+    
+    hgt_sc[[jj]] <- h
     
     colnames(dd) <- colnames(dt)
     out2[[jj]] <- dd
   }
   
-  out2_tot[[i]] <- out2
+  out_tot[[i]] <- out2
 }
 
-
-
-
-#________________________________________
-
-
-save(out1_tot, out2_tot, file = "clean.data/scaling-abund.rda")
-save(hgt_sc, hgt_sc2, file = "clean.data/scaling-hgt.rda")
+save(out_tot, file = "clean.data/scaling-abund.rda")
+save(hgt_sc, file = "clean.data/scaling-hgt.rda")
