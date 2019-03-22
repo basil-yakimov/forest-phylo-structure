45aed3915240c97cab757db95ae89ee1082ad925
basil-yakimov <damselfly@yandex.ru>
2019-03-22 17:07:10 +0300

    back to taxa.labels null

--- 05.scaling-phylogenetic-analysis.r
+++ 05.scaling-phylogenetic-analysis.r
@@ -15,18 +15,18 @@ ta_sc_ses <- sa_sc_ses <- ha_sc_ses <- vector(mode = "list", length = 20)
 
 for (jj in 1:20)
 {
-  ses <- ses.mpd(ta_sc[[jj]], wood.mat, null.model = "richness")
-  ses.a <- ses.mpd(ta_sc[[jj]], wood.mat, null.model = "richness", abundance.weighted = T)
+  ses <- ses.mpd(ta_sc[[jj]], wood.mat, null.model = "taxa.labels", runs = 9999)
+  ses.a <- ses.mpd(ta_sc[[jj]], wood.mat, null.model = "taxa.labels", runs = 9999, abundance.weighted = T)
   ta_sc_ses[[jj]] <- cbind(z = ses$mpd.obs.z, p = ses$mpd.obs.p,
                            z.a = ses.a$mpd.obs.z, p.a = ses.a$mpd.obs.p)
   
-  ses <- ses.mpd(sa_sc[[jj]], wood.mat, null.model = "richness")
-  ses.a <- ses.mpd(sa_sc[[jj]], wood.mat, null.model = "richness", abundance.weighted = T)
+  ses <- ses.mpd(sa_sc[[jj]], wood.mat, null.model = "taxa.labels", runs = 9999)
+  ses.a <- ses.mpd(sa_sc[[jj]], wood.mat, null.model = "taxa.labels", runs = 9999, abundance.weighted = T)
   sa_sc_ses[[jj]] <- cbind(z = ses$mpd.obs.z, p = ses$mpd.obs.p,
                            z.a = ses.a$mpd.obs.z, p.a = ses.a$mpd.obs.p)
   
-  ses <- ses.mpd(ha_sc[[jj]], herb.mat, null.model = "richness")
-  ses.a <- ses.mpd(ha_sc[[jj]], herb.mat, null.model = "richness", abundance.weighted = T)
+  ses <- ses.mpd(ha_sc[[jj]], herb.mat, null.model = "taxa.labels", runs = 9999)
+  ses.a <- ses.mpd(ha_sc[[jj]], herb.mat, null.model = "taxa.labels", runs = 9999, abundance.weighted = T)
   ha_sc_ses[[jj]] <- cbind(z = ses$mpd.obs.z, p = ses$mpd.obs.p,
                            z.a = ses.a$mpd.obs.z, p.a = ses.a$mpd.obs.p)
 }  
