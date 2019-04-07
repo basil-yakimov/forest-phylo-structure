c46b46f76216fb83d4b4a10238e77ded51c776fc
arrirh <arrirh@gmail.com>
2019-04-07 22:35:36 +0300

    plots and stats

--- 03b.stats-n-plotting.r
+++ 03b.stats-n-plotting.r
@@ -99,3 +99,99 @@ for (i in 2:ncol(t.ses.z)){
 par(op)
 
 dev.off()
+
+
+#_____________________________________________________________________
+#
+
+png("figures/tree-nti.png", 600, 400)
+
+par(mar = c(4.5, 4.5, 1.5, 1))
+
+NRI <- -t.ses.z[, 2]
+plot(hgt, NRI, xlab = "Высота над уровнем моря, м", cex = 2, cex.axis = 2, cex.lab = 2, pch = 21, bg = "tomato")
+abline(lm(NRI ~ hgt), lwd = 2)
+points(hgt[t.ses.p[, 2] < 0.05 | t.ses.p[, 2] > 0.95],
+       NRI[t.ses.p[, 2] < 0.05 | t.ses.p[, 2] > 0.95],
+       cex = 2, pch = 21, bg = "darkred")
+
+dev.off()
+
+
+png("figures/shrub-nti.png", 600, 400)
+
+par(mar = c(4.5, 4.5, 1.5, 1))
+
+NRI <- -s.ses.z[, 2]
+plot(hgt, NRI, xlab = "Высота над уровнем моря, м", cex = 2, cex.axis = 2, cex.lab = 2, pch = 21, bg = "skyblue")
+points(hgt[s.ses.p[, 2] < 0.05 | s.ses.p[, 2] > 0.95],
+       NRI[s.ses.p[, 2] < 0.05 | s.ses.p[, 2] > 0.95],
+       cex = 2, pch = 21, bg = "darkblue")
+
+dev.off()
+
+
+png("figures/herb-nti.png", 600, 400)
+
+par(mar = c(4.5, 4.5, 1.5, 1))
+
+NRI <- -h.ses.z[, 2]
+plot(hgt, NRI, xlab = "Высота над уровнем моря, м", cex = 2, cex.axis = 2, cex.lab = 2, pch = 21, bg = "limegreen")
+points(hgt[h.ses.p[, 2] < 0.05 | h.ses.p[, 2] > 0.95],
+       NRI[h.ses.p[, 2] < 0.05 | h.ses.p[, 2] > 0.95],
+       cex = 2, pch = 21, bg = "darkgreen")
+
+dev.off()
+
+
+#_____________________________________________________________________
+#
+
+output <- matrix(0, nrow = 14, ncol = 3)
+colnames(output) <- c("spearman", "p-value", "r^2")
+
+for (i in 2:ncol(t.ses.z)){
+  NRI <- -t.ses.z[, i]
+  
+  output[i-1, 1] <- round(cor.test(hgt, NRI, method = "spearman")$estimate, 3)
+  output[i-1, 2] <- round(cor.test(hgt, NRI, method = "spearman", use = "complete")$p.value, 3)
+  output[i-1, 3] <- round(cor.test(hgt, NRI)$estimate^2, 3)
+  
+}
+
+rownames(output) <- colnames(t.ses.z)[2:15]
+write.csv2(output, file = "figures/tree-nti.csv")
+
+
+
+output <- matrix(0, nrow = 14, ncol = 3)
+colnames(output) <- c("spearman", "p-value", "r^2")
+
+for (i in 2:ncol(s.ses.z)){
+  NRI <- -s.ses.z[, i]
+  
+  output[i-1, 1] <- round(cor.test(hgt, NRI, method = "spearman")$estimate, 3)
+  output[i-1, 2] <- round(cor.test(hgt, NRI, method = "spearman", use = "complete")$p.value, 3)
+  output[i-1, 3] <- round(cor.test(hgt, NRI)$estimate^2, 3)
+  
+}
+
+rownames(output) <- colnames(t.ses.z)[2:15]
+write.csv2(output, file = "figures/shrub-nti.csv")
+
+
+
+output <- matrix(0, nrow = 14, ncol = 3)
+colnames(output) <- c("spearman", "p-value", "r^2")
+
+for (i in 2:ncol(h.ses.z)){
+  NRI <- -h.ses.z[, i]
+  
+  output[i-1, 1] <- round(cor.test(hgt, NRI, method = "spearman")$estimate, 3)
+  output[i-1, 2] <- round(cor.test(hgt, NRI, method = "spearman", use = "complete")$p.value, 3)
+  output[i-1, 3] <- round(cor.test(hgt, NRI)$estimate^2, 3)
+  
+}
+
+rownames(output) <- colnames(t.ses.z)[2:15]
+write.csv2(output, file = "figures/herb-nti.csv")
