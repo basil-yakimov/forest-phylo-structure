e75a9c532eda17eaa64838fe5db2f9bbbe72cf9c
Alina Gerasimova <43878627+arrirh@users.noreply.github.com>
2019-04-07 22:38:28 +0300

    new plots and stats  (#15)
    *  multiple null modelsc support in ses computation  (#1)
    
    * multiple null modelsc support in ses computation
    
    * multiple nulls plotting
    
    * update aw scaling plot
    
    * split phylogeny between trees and shrubs
    
    * .
    
    *  add philogenetic hill numbers  (#2)
    
    * multiple null modelsc support in ses computation
    
    * multiple nulls plotting
    
    * update aw scaling plot
    
    * split phylogeny between trees and shrubs
    
    * first draft of conference figures
    
    * add philogenetic hill numbers
    
    *  rename dev script  (#3)
    
    * multiple null modelsc support in ses computation
    
    * multiple nulls plotting
    
    * update aw scaling plot
    
    * split phylogeny between trees and shrubs
    
    * first draft of conference figures
    
    * add philogenetic hill numbers
    
    * attempt to use vegan oecosimu for null model
    
    * move to picante indswap null
    
    * rename dev script
    
    * plots and stats

--- 03.stats-n-plotting.r
+++ 03.stats-n-plotting.r
@@ -100,3 +100,101 @@ for (i in 2:ncol(t.ses.z)){
 par(op)
 
 dev.off()
+
+#_____________________________________________________________________
+#
+
+png("figures/tree-nri.png", 600, 400)
+
+par(mar = c(4.5, 4.5, 1.5, 1))
+
+NRI <- -t.ses.z[, 2]
+plot(hgt, NRI, xlab = "Высота над уровнем моря, м", cex = 2, cex.axis = 2, cex.lab = 2, pch = 21, bg = "tomato")
+abline(lm(NRI ~ hgt), lwd = 2)
+points(hgt[t.ses.p[, 2] < 0.05 | t.ses.p[, 2] > 0.95],
+       NRI[t.ses.p[, 2] < 0.05 | t.ses.p[, 2] > 0.95],
+       cex = 2, pch = 21, bg = "darkred")
+
+dev.off()
+
+
+png("figures/shrub-nri.png", 600, 400)
+
+par(mar = c(4.5, 4.5, 1.5, 1))
+
+NRI <- -s.ses.z[, 2]
+plot(hgt, NRI, xlab = "Высота над уровнем моря, м", cex = 2, cex.axis = 2, cex.lab = 2, pch = 21, bg = "skyblue")
+abline(lm(NRI ~ hgt), lwd = 2)
+points(hgt[s.ses.p[, 2] < 0.05 | s.ses.p[, 2] > 0.95],
+       NRI[s.ses.p[, 2] < 0.05 | s.ses.p[, 2] > 0.95],
+       cex = 2, pch = 21, bg = "darkblue")
+
+dev.off()
+
+
+png("figures/herb-nri.png", 600, 400)
+
+par(mar = c(4.5, 4.5, 1.5, 1))
+
+NRI <- -h.ses.z[, 2]
+plot(hgt, NRI, xlab = "Высота над уровнем моря, м", cex = 2, cex.axis = 2, cex.lab = 2, pch = 21, bg = "limegreen")
+abline(lm(NRI ~ hgt), lwd = 2)
+points(hgt[h.ses.p[, 2] < 0.05 | h.ses.p[, 2] > 0.95],
+       NRI[h.ses.p[, 2] < 0.05 | h.ses.p[, 2] > 0.95],
+       cex = 2, pch = 21, bg = "darkgreen")
+
+dev.off()
+
+
+#_____________________________________________________________________
+#
+
+output <- matrix(0, nrow = 14, ncol = 3)
+colnames(output) <- c("spearman", "p-value", "r^2")
+
+for (i in 2:ncol(t.ses.z)){
+  NRI <- -t.ses.z[, i]
+  
+  output[i-1, 1] <- round(cor.test(hgt, NRI, method = "spearman")$estimate, 3)
+  output[i-1, 2] <- round(cor.test(hgt, NRI, method = "spearman", use = "complete")$p.value, 3)
+  output[i-1, 3] <- round(cor.test(hgt, NRI)$estimate^2, 3)
+
+}
+
+rownames(output) <- colnames(t.ses.z)[2:15]
+write.csv2(output, file = "figures/tree-nri.csv")
+
+
+
+output <- matrix(0, nrow = 14, ncol = 3)
+colnames(output) <- c("spearman", "p-value", "r^2")
+
+for (i in 2:ncol(s.ses.z)){
+  NRI <- -s.ses.z[, i]
+  
+  output[i-1, 1] <- round(cor.test(hgt, NRI, method = "spearman")$estimate, 3)
+  output[i-1, 2] <- round(cor.test(hgt, NRI, method = "spearman", use = "complete")$p.value, 3)
+  output[i-1, 3] <- round(cor.test(hgt, NRI)$estimate^2, 3)
+  
+}
+
+rownames(output) <- colnames(t.ses.z)[2:15]
+write.csv2(output, file = "figures/shrub-nri.csv")
+
+
+
+output <- matrix(0, nrow = 14, ncol = 3)
+colnames(output) <- c("spearman", "p-value", "r^2")
+
+for (i in 2:ncol(h.ses.z)){
+  NRI <- -h.ses.z[, i]
+  
+  output[i-1, 1] <- round(cor.test(hgt, NRI, method = "spearman")$estimate, 3)
+  output[i-1, 2] <- round(cor.test(hgt, NRI, method = "spearman", use = "complete")$p.value, 3)
+  output[i-1, 3] <- round(cor.test(hgt, NRI)$estimate^2, 3)
+  
+}
+
+rownames(output) <- colnames(t.ses.z)[2:15]
+write.csv2(output, file = "figures/herb-nri.csv")
+
