f4d6ff591bd718295ef5791f2682c65a534e9e9c
arrirh <arrirh@gmail.com>
2019-06-13 14:41:14 +0300

    fixed BCI phylo

--- bci.r
+++ bci.r
@@ -19,15 +19,35 @@ for (i in 1:nrow(tax2)){
   }
 }
 
+bcispp <- remove.factors(bcispp)
+for (i in 1:nrow(tax)){
+  if (tax$Current[i] %in% bcispp$spp){
+    id <- which(bcispp$spp %in% tax$Current[i])
+    f <- as.character(tax$Family[i])
+    bcispp$family[id] <- f
+  }
+}
+
 bcispp$spp <- sapply(strsplit(bcispp$spp, " "), function(x) paste(x[1], x[2]))
 bcispp$spp <- sapply(strsplit(bcispp$spp, "_"), function(x) paste(x[1]))
 bcispp$spp <- sub(" ", "_", bcispp$spp)
-bcispp <- remove.factors(bcispp)
 bcispp$family[grep("Fabaceae*", bcispp$family)] <- "Fabaceae"
   
 id <- which(bcispp$spp %in% GBOTB.extended$tip.label)
 bcispp$spp[-id]
 bcispp <- bcispp[-300, ]
+
+
+for (i in 1:nrow(bcispp)){
+  if (bcispp$genus[i] %in% tips.info$genus){
+    id <- grep(bcispp$genus[i], tips.info$genus)[1]
+    if (!(bcispp$family[i] == tips.info$family[id])){
+      bcispp$family[i] <- tips.info$family[id]
+    }
+  } 
+}
+
+bcispp$family[292] <- "Cannabaceae"
 
 sp.list <- data.frame(species = bcispp$spp, genus = bcispp$genus, family = bcispp$family)
 bci.tree <- phylo.maker(sp.list = sp.list, scenarios="S1")
@@ -35,3 +55,4 @@ bci.tree <- phylo.maker(sp.list = sp.list, scenarios="S1")
 
 bci.tree <- bci.tree$scenario.1
 save(bci.tree, file = "clean.data/bci-phylo.rda")
+
