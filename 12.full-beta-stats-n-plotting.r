e59da65439134ef3b4c1de06ebbad528fa9ba430
basil-yakimov <damselfly@yandex.ru>
2019-07-08 14:19:12 +0300

    add figure production for beta analysis

--- 12.full-beta-stats-n-plotting.r
+++ 12.full-beta-stats-n-plotting.r
@@ -1,22 +1,9 @@
 load("clean.data/beta.full.rda")
 
-
-plot(tt0 ~ dh, data = beta, pch = 19, col = rgb(0.25, 0.75, 0.25, 0.3))
-plot(tt1 ~ dh, data = beta, pch = 19, col = rgb(0.25, 0.75, 0.25, 0.3))
-plot(tt2 ~ dh, data = beta, pch = 19, col = rgb(0.25, 0.75, 0.25, 0.3))
-
-plot(st0 ~ dh, data = beta, pch = 19, col = rgb(0.25, 0.25, 0.75, 0.3))
-plot(st1 ~ dh, data = beta, pch = 19, col = rgb(0.25, 0.25, 0.75, 0.3))
-plot(st2 ~ dh, data = beta, pch = 19, col = rgb(0.25, 0.25, 0.75, 0.3))
-
-plot(ht0 ~ dh, data = beta, pch = 19, col = rgb(0.75, 0.25, 0.25, 0.3))
-plot(ht1 ~ dh, data = beta, pch = 19, col = rgb(0.75, 0.25, 0.25, 0.3))
-plot(ht2 ~ dh, data = beta, pch = 19, col = rgb(0.75, 0.25, 0.25, 0.3))
-
 source("R/plot.ses.r")
 
-
-op <- par(mfcol = c(3, 3), mar = c(4,4,0.5,0.5))
+png("figures/beta-tax-vs-dh.png", width = 3000, height = 2000)
+op <- par(mfcol = c(3, 3), mar = c(4, 4, 0.5, 0.5), cex = 2)
 
 plot.ses(beta$tt0, beta$dh, col = rgb(0.25, 0.75, 0.25, 0.3), lab = expression(beta[0]), xlab = expression(Delta * "hgt"))
 plot.ses(beta$tt1, beta$dh, col = rgb(0.25, 0.75, 0.25, 0.3), lab = expression(beta[1]), xlab = expression(Delta * "hgt"))
@@ -31,10 +18,11 @@ plot.ses(beta$ht1, beta$dh, col = rgb(0.75, 0.25, 0.25, 0.3), lab = expression(b
 plot.ses(beta$ht2, beta$dh, col = rgb(0.75, 0.25, 0.25, 0.3), lab = expression(beta[2]), xlab = expression(Delta * "hgt"))
 
 par(op)
+dev.off()
 
 
-
-op <- par(mfcol = c(3, 3), mar = c(4,4,0.5,0.5))
+png("figures/beta-tax-vs-mh.png", width = 3000, height = 2000)
+op <- par(mfcol = c(3, 3), mar = c(4, 4, 0.5, 0.5), cex = 2)
 
 plot.ses(beta$tt0, beta$mh, col = rgb(0.25, 0.75, 0.25, 0.3), lab = expression(beta[0]), xlab = "mean hgt")
 plot.ses(beta$tt1, beta$mh, col = rgb(0.25, 0.75, 0.25, 0.3), lab = expression(beta[1]), xlab = "mean hgt")
@@ -49,10 +37,12 @@ plot.ses(beta$ht1, beta$mh, col = rgb(0.75, 0.25, 0.25, 0.3), lab = expression(b
 plot.ses(beta$ht2, beta$mh, col = rgb(0.75, 0.25, 0.25, 0.3), lab = expression(beta[2]), xlab = "mean hgt")
 
 par(op)
+dev.off()
 
 #---#
 
-op <- par(mfcol = c(3, 3), mar = c(4,4,0.5,0.5))
+png("figures/beta-phylo-vs-dh.png", width = 3000, height = 2000)
+op <- par(mfcol = c(3, 3), mar = c(4, 4, 0.5, 0.5), cex = 2)
 
 plot.ses(beta$tp0, beta$dh, col = rgb(0.25, 0.75, 0.25, 0.3), lab = expression("phylo - " * beta[0]), xlab = expression(Delta * "hgt"))
 plot.ses(beta$tp1, beta$dh, col = rgb(0.25, 0.75, 0.25, 0.3), lab = expression("phylo - " * beta[1]), xlab = expression(Delta * "hgt"))
@@ -67,9 +57,11 @@ plot.ses(beta$hp1, beta$dh, col = rgb(0.75, 0.25, 0.25, 0.3), lab = expression("
 plot.ses(beta$hp2, beta$dh, col = rgb(0.75, 0.25, 0.25, 0.3), lab = expression("phylo - " * beta[2]), xlab = expression(Delta * "hgt"))
 
 par(op)
+dev.off()
 
 
-op <- par(mfcol = c(3, 3), mar = c(4,4,0.5,0.5))
+png("figures/beta-phylo-vs-mh.png", width = 3000, height = 2000)
+op <- par(mfcol = c(3, 3), mar = c(4, 4, 0.5, 0.5), cex = 2)
 
 plot.ses(beta$tp0, beta$mh, col = rgb(0.25, 0.75, 0.25, 0.3), lab = expression("phylo - " * beta[0]), xlab = "mean hgt")
 plot.ses(beta$tp1, beta$mh, col = rgb(0.25, 0.75, 0.25, 0.3), lab = expression("phylo - " * beta[1]), xlab = "mean hgt")
@@ -84,10 +76,12 @@ plot.ses(beta$hp1, beta$mh, col = rgb(0.75, 0.25, 0.25, 0.3), lab = expression("
 plot.ses(beta$hp2, beta$mh, col = rgb(0.75, 0.25, 0.25, 0.3), lab = expression("phylo - " * beta[2]), xlab = "mean hgt")
 
 par(op)
+dev.off()
 
 #---#
 
-op <- par(mfcol = c(3, 3), mar = c(4,4,0.5,0.5))
+png("figures/beta-phylo-vs-tax.png", width = 3000, height = 2000)
+op <- par(mfcol = c(3, 3), mar = c(4, 4, 0.5, 0.5), cex = 2)
 
 plot(beta$tp0 ~ beta$tt0, pch = 19, col = rgb(0.25, 0.75, 0.25, 0.3), ylab = expression("phylo - " * beta[0]), xlab = expression(beta[0]))
 abline(0, 1)
@@ -153,10 +147,11 @@ abline(fit, lty = 2, lwd = 2, col = "grey")
 beta$hp2.res <- beta$hp2 - predict(fit)
 
 par(op)
+dev.off()
 
 
-
-op <- par(mfcol = c(3, 3), mar = c(4,4,0.5,0.5))
+png("figures/beta-phylo-res-hist.png", width = 3000, height = 2000)
+op <- par(mfcol = c(3, 3), mar = c(4, 4, 0.5, 0.5), cex = 2)
 
 hist(beta$tp0.res, col = rgb(0.25, 0.75, 0.25, 0.3), xlab = expression("res - phylo - " * beta[0]), main = NULL)
 abline(v = 0)
@@ -195,10 +190,11 @@ abline(v = 0)
 abline(v = mean(beta$hp2.res), lwd = 2, col = "grey")
 
 par(op)
+dev.off()
 
 
-
-op <- par(mfcol = c(3, 3), mar = c(4,4,0.5,0.5))
+png("figures/beta-phylo-ses-hist.png", width = 3000, height = 2000)
+op <- par(mfcol = c(3, 3), mar = c(4, 4, 0.5, 0.5), cex = 2)
 
 hist(beta$tp0.ses, col = rgb(0.25, 0.75, 0.25, 0.3), xlab = expression("ses - phylo - " * beta[0]), main = NULL)
 abline(v = 0)
@@ -237,10 +233,11 @@ abline(v = 0)
 abline(v = mean(beta$hp2.ses), lwd = 2, col = "grey")
 
 par(op)
+dev.off()
 
 
-
-op <- par(mfcol = c(3, 3), mar = c(4,4,0.5,0.5))
+png("figures/beta-res-vs-ses.png", width = 3000, height = 2000)
+op <- par(mfcol = c(3, 3), mar = c(4, 4, 0.5, 0.5), cex = 2)
 
 plot.ses(beta$tp0.ses, beta$tp0.res, col = rgb(0.25, 0.75, 0.25, 0.3), lab = expression("res - phylo - " * beta[0]), xlab = expression("ses - phylo - " * beta[0]))
 plot.ses(beta$tp1.ses, beta$tp1.res, col = rgb(0.25, 0.75, 0.25, 0.3), lab = expression("res - phylo - " * beta[1]), xlab = expression("ses - phylo - " * beta[1]))
@@ -255,11 +252,12 @@ plot.ses(beta$hp1.ses, beta$hp1.res, col = rgb(0.75, 0.25, 0.25, 0.3), lab = exp
 plot.ses(beta$hp2.ses, beta$hp2.res, col = rgb(0.75, 0.25, 0.25, 0.3), lab = expression("res - phylo - " * beta[2]), xlab = expression("ses - phylo - " * beta[2]))
 
 par(op)
-
+dev.off()
 
 #---#
 
-op <- par(mfcol = c(3, 3), mar = c(4,4,0.5,0.5))
+png("figures/beta-res-vs-dh.png", width = 3000, height = 2000)
+op <- par(mfcol = c(3, 3), mar = c(4, 4, 0.5, 0.5), cex = 2)
 
 plot.ses(beta$tp0.res, beta$dh, col = rgb(0.25, 0.75, 0.25, 0.3), lab = expression("res - phylo - " * beta[0]), xlab = expression(Delta * "hgt"))
 plot.ses(beta$tp1.res, beta$dh, col = rgb(0.25, 0.75, 0.25, 0.3), lab = expression("res - phylo - " * beta[1]), xlab = expression(Delta * "hgt"))
@@ -274,9 +272,10 @@ plot.ses(beta$hp1.res, beta$dh, col = rgb(0.75, 0.25, 0.25, 0.3), lab = expressi
 plot.ses(beta$hp2.res, beta$dh, col = rgb(0.75, 0.25, 0.25, 0.3), lab = expression("res - phylo - " * beta[2]), xlab = expression(Delta * "hgt"))
 
 par(op)
+dev.off()
 
-
-op <- par(mfcol = c(3, 3), mar = c(4,4,0.5,0.5))
+png("figures/beta-res-vs-mh.png", width = 3000, height = 2000)
+op <- par(mfcol = c(3, 3), mar = c(4, 4, 0.5, 0.5), cex = 2)
 
 plot.ses(beta$tp0.res, beta$mh, col = rgb(0.25, 0.75, 0.25, 0.3), lab = expression("res - phylo - " * beta[0]), xlab = "mean hgt")
 plot.ses(beta$tp1.res, beta$mh, col = rgb(0.25, 0.75, 0.25, 0.3), lab = expression("res - phylo - " * beta[1]), xlab = "mean hgt")
@@ -291,10 +290,11 @@ plot.ses(beta$hp1.res, beta$mh, col = rgb(0.75, 0.25, 0.25, 0.3), lab = expressi
 plot.ses(beta$hp2.res, beta$mh, col = rgb(0.75, 0.25, 0.25, 0.3), lab = expression("res - phylo - " * beta[2]), xlab = "mean hgt")
 
 par(op)
+dev.off()
 
 
-
-op <- par(mfcol = c(3, 3), mar = c(4,4,0.5,0.5))
+png("figures/beta-ses-vs-dh.png", width = 3000, height = 2000)
+op <- par(mfcol = c(3, 3), mar = c(4, 4, 0.5, 0.5), cex = 2)
 
 plot.ses(beta$tp0.ses, beta$dh, col = rgb(0.25, 0.75, 0.25, 0.3), lab = expression("ses - phylo - " * beta[0]), xlab = expression(Delta * "hgt"))
 plot.ses(beta$tp1.ses, beta$dh, col = rgb(0.25, 0.75, 0.25, 0.3), lab = expression("ses - phylo - " * beta[1]), xlab = expression(Delta * "hgt"))
@@ -309,9 +309,11 @@ plot.ses(beta$hp1.ses, beta$dh, col = rgb(0.75, 0.25, 0.25, 0.3), lab = expressi
 plot.ses(beta$hp2.ses, beta$dh, col = rgb(0.75, 0.25, 0.25, 0.3), lab = expression("ses - phylo - " * beta[2]), xlab = expression(Delta * "hgt"))
 
 par(op)
+dev.off()
 
 
-op <- par(mfcol = c(3, 3), mar = c(4,4,0.5,0.5))
+png("figures/beta-ses-vs-mh.png", width = 3000, height = 2000)
+op <- par(mfcol = c(3, 3), mar = c(4, 4, 0.5, 0.5), cex = 2)
 
 plot.ses(beta$tp0.ses, beta$mh, col = rgb(0.25, 0.75, 0.25, 0.3), lab = expression("ses - phylo - " * beta[0]), xlab = "mean hgt")
 plot.ses(beta$tp1.ses, beta$mh, col = rgb(0.25, 0.75, 0.25, 0.3), lab = expression("ses - phylo - " * beta[1]), xlab = "mean hgt")
@@ -326,3 +328,37 @@ plot.ses(beta$hp1.ses, beta$mh, col = rgb(0.75, 0.25, 0.25, 0.3), lab = expressi
 plot.ses(beta$hp2.ses, beta$mh, col = rgb(0.75, 0.25, 0.25, 0.3), lab = expression("ses - phylo - " * beta[2]), xlab = "mean hgt")
 
 par(op)
+dev.off()
+
+
+
+#----------------------------------#
+
+library(vegan)
+
+my.as.dist <- function(x)
+{
+  dddd <- matrix(0, nrow = 96, ncol = 96)
+  for (ii in 1:95) dddd[(ii+1):(96), ii] <- x[(sum(95:(95-ii+1))-(95-ii+1)+1):(sum(95:(95-ii+1)))]
+  as.dist(dddd)
+}
+
+
+mantel(my.as.dist(beta$sp1), beta$st1)
+mantel(my.as.dist(beta$sp1), beta$dh)
+
+mantel.partial(my.as.dist(beta$sp1), beta$dh, beta$st1)
+
+mantel(my.as.dist(beta$sp1.ses), beta$dh)
+
+cor(beta$sp1.ses, beta$dh)
+
+
+
+library(lmPerm)
+
+fit <- lmp(hp2 ~ as.vector(ht2) + as.vector(dh) + as.vector(mh), beta)
+summary(fit)
+
+fit <- lmp(hp2 ~ as.vector(dh) * as.vector(mh), beta)
+summary(fit)
