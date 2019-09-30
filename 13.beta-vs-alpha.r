71a9e3069672fb49ffa903786fb0ed665e979fbe
basil-yakimov <damselfly@yandex.ru>
2019-09-30 10:56:39 +0300

    add residual beta vs hgt

--- 13.beta-vs-alpha.r
+++ 13.beta-vs-alpha.r
@@ -46,3 +46,23 @@ plot.ses(beta.ses$hd0[c(T, F)], (ha.ses$mpd[seq(1, 95, by = 2)] + ha.ses$mpd[seq
 plot.ses(beta.ses$hd1[c(T, F)], (ha.ses$mpd.a[seq(1, 95, by = 2)] + ha.ses$mpd.a[seq(2, 96, by = 2)])/2, 
          col = "forestgreen", lab = expression("SES D'"[pw]), xlab = expression("Mean SES MPD"[a]))
 par(op)
+
+#---#
+
+plot.res <- function(alpha, beta, col, lab = "")
+{
+  fit <- lm(beta[c(T, F)] ~ I((alpha[seq(1, 95, by = 2)] + alpha[seq(2, 96, by = 2)])/2))
+  res <- resid(fit)
+  plot.ses(res, hgt[c(T, F)][as.numeric(names(res))], col = col, lab = lab)
+}
+
+op <- par(mfcol = c(2, 3))
+plot.res(ta.ses$mpd, beta.ses$td0, col = "tomato", lab = expression("residual SES D"[pw]))
+plot.res(ta.ses$mpd.a, beta.ses$td1, col = "tomato", lab = expression("residual SES D'"[pw]))
+
+plot.res(sa.ses$mpd, beta.ses$sd0, col = "skyblue", lab = expression("residual SES D"[pw]))
+plot.res(sa.ses$mpd.a, beta.ses$sd1, col = "skyblue", lab = expression("residual SES D'"[pw]))
+
+plot.res(ha.ses$mpd, beta.ses$hd0, col = "forestgreen", lab = expression("residual SES D"[pw]))
+plot.res(ha.ses$mpd.a, beta.ses$hd1, col = "forestgreen", lab = expression("residual SES D'"[pw]))
+par(op)
